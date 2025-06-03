a1d3af92142164f51caaec789c17b98bd4d4c40a
StefanikJakub <jkbstfnk@gmail.com>
2025-06-04 00:47:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using kalendar.Views;
+using System;
+using System.Windows.Forms;
+
+namespace kalendar
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Prihlasenie()); // Spustí prihlasovací formulár
+        }
+    }
+}
